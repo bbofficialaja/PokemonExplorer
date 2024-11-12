b3f9fe9b8208d034ce992165f9c35268715ed81e
BB Official <bbofficialaja@gmail.com>
2024-11-12 18:24:45 +0700

    menambahkan index pada screen supaya mudah di import

---
+++ src/screens/index.js
@@ -0,0 +1,5 @@
+// src/screens/index.js
+export { default as HomeScreen } from "./HomeScreen";
+export { default as DetailScreen } from "./DetailScreen";
+export { default as SearchScreen } from "./SearchScreen";
+export { default as FavoritesScreen } from "./FavoritesScreen";
